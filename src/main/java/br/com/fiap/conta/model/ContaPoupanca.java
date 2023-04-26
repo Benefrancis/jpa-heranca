7812020a835f6f37e09eaae5d5eb26882a24b9b6
Benefrancis <benefrancis@gmail.com>
2023-04-26 11:06:19 -0300

    Implementamos as classes Conta, ContaCorrente e ContaPoupanca

---
+++ src/main/java/br/com/fiap/conta/model/ContaPoupanca.java
@@ -0,0 +1,15 @@
+package br.com.fiap.conta.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContaPoupanca extends Conta {
+
+    private Integer aniversario;
+
+
+}
